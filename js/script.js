a35f1491149f2fa6a710899e1bb0fef92d973a44
José Eugenio Navarro Bovi <jose.navarrobovi@alu.frt.utn.edu.ar>
2023-11-27 02:08:07 -0300

    Se realizo la pagina web

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var intervaloCronometro;
+    var horas = 0;
+    var minutos = 0;
+    var segundos = 0;
+
+    function iniciarCronometro() {
+      intervaloCronometro = setInterval(actualizarCronometro, 1000);
+    }
+
+    function reiniciarCronometro() {
+      clearInterval(intervaloCronometro);
+      horas = 0;
+      minutos = 0;
+      segundos = 0;
+      actualizarCronometro();
+    }
+
+    function pausarCronometro() {
+      clearInterval(intervaloCronometro);
+    }
+
+    function actualizarCronometro() {
+      segundos++;
+      if (segundos >= 60) {
+        segundos = 0;
+        minutos++;
+        if (minutos >= 60) {
+          minutos = 0;
+          horas++;
+        }
+      }
+      var timeString = pad(horas) + ":" + pad(minutos) + ":" + pad(segundos);
+      document.querySelector(".cronometro").textContent = timeString;
+    }
+
+    function pad(value) {
+      return value.toString().padStart(2, "0");
+    }
